f5d8002be119ef1b46b49d2a7703e8e10e77f973
reizer <reizer911@gmail.com>
2020-06-22 23:04:07 +0300

    Initial Commit

---
+++ Lendmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lendmarks
+//
+//  Created by DMITRY LESHCHENOK on 22.06.2020.
+//  Copyright © 2020 DMITRY LESHCHENOK. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
